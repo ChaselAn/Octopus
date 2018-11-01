19299109fd320f302dc7ecf7cde157b0b89926f9
ancheng <cheng.an@duodian.com>
2018-11-01 17:17:30 +0800

    init

---
+++ Octopus/Octopus.h
@@ -0,0 +1,19 @@
+//
+//  Octopus.h
+//  Octopus
+//
+//  Created by ancheng on 2018/11/1.
+//  Copyright © 2018年 chaselan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Octopus.
+FOUNDATION_EXPORT double OctopusVersionNumber;
+
+//! Project version string for Octopus.
+FOUNDATION_EXPORT const unsigned char OctopusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Octopus/PublicHeader.h>
+
+
